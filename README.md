d27c99fdbc4423bc6b9ddbf5f9e270932c70e9cc
Steven Maglio <smaglio81@gmail.com>
2018-07-14 10:25:02 -0700

    Adding script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # powershell-mygetucsb
 MyGetUcsb module for Powershell (out of date and not actively supported)
+
+This module was developed to populate and set permissions on developer accounts in myget. It won't be maintained, but hopefully can use it to make something better later on.
+
+See `Scripts/20180702-Populate-MyGet-Users.ps1` for the population script
---
+++ Scripts/20180702-Populate-MyGet-Users.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module MyGetUcsb -Force
+Import-Module ActiveDirectory
+
+$developers = Get-ADGroupMember -Identity "Developers"
+$domainAdmins = Get-ADGroupMember -Identity "Domain Admins"
+
+$dgl = $developers |? { $domainAdmins.SamAccountName -notcontains $_.SamAccountName }
+$dgl = $dgl |? { $_.name -notmatch "- Admin" }
+$dgl = $dgl |? { @("names", "of", "people") -notcontains $_.name }
+
+$dgl | ft -AutoSize
+$dgl.Count
+
+$users = @()
+foreach($d in $dgl) {
+    $nameSplit = $d.name.Split(" ", [StringSplitOptions]::RemoveEmptyEntries)
+    $email = "{0}.{1}@sa.ucsb.edu" -f $nameSplit[0], $nameSplit[$nameSplit.Count - 1]
+    $email = $email.ToLower()
+
+    $user = New-Object PSCustomObject -Property @{
+        Username = $d.SamAccountName.ToLower();
+        Name = $d.name;
+        Email = $email
+    }
+    $users += @($user)
+}
+
+$users | ft -AutoSize
+$users.Count
+
+Import-Module PowerShellLogging
+$log = Enable-LogFile -Path D:\Projects\Dev\Enterprise\Powershell\MyGetUcsb\Scripts\20180702-Populate-MyGet-Users.log
+try {
+    foreach($user in $users) {
+        New-MyGetUser -Username $user.Username -Name $user.Name -Email $user.Email
+        Set-MyGetFeedUserPrivilege -Feed public -Username $user.Username -Privilege FeedContributor
+        Set-MyGetFeedUserPrivilege -Feed internal -Username $user.Username -Privilege FeedContributor
+        Set-MyGetFeedUserPrivilege -Feed internal-nonprod -Username $user.Username -Privilege FeedContributor
+    }
+} finally {
+    $log | Disable-LogFile
+}
